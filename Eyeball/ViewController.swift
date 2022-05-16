8bf4b3aa3a1a6647aa106b79d54c9e2d7816a0ce
WenJie Qiu <qiuwenjie1997@163.com>
2022-05-16 22:37:24 +0800

    finish!

--- Eyeball/ViewController.swift
+++ Eyeball/ViewController.swift
@@ -7,16 +7,31 @@
 
 import UIKit
 import SnapKit
+import Foundation
 
 class ViewController: UIViewController {
-    
     lazy var hitBtn = makeHitButton()
-
+    lazy var sliderBar = makeSliderBar()
+    lazy var titleLabel = makeTitleLabel()
+    lazy var resetBtn = makeResetButton()
+    lazy var scoreLabel = makeScoreLabel()
+    lazy var backgroundImageView = makeBackgroundImageView()
+    
+    var randomNumber: Int = Int.random(in: 1...100) {
+        didSet {
+            titleLabel.text = "Put the Bull's Eye as close as you can to:\(randomNumber)"
+        }
+    }
+    var totalScore: Int = 0 {
+        didSet {
+            scoreLabel.text = "Current Score: \(totalScore)"
+        }
+    }
+    var numberOfAttemps: Int = 10
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setupUI()
-        // Do any additional setup after loading the view.
     }
     
     func setupUI() {
@@ -24,9 +39,43 @@ class ViewController: UIViewController {
         hitBtn.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.width.equalTo(200)
-            make.height.equalTo(100)
+            make.height.equalTo(70)
             make.bottom.equalToSuperview().offset(-200)
         }
+        
+        view.addSubview(resetBtn)
+        resetBtn.snp.makeConstraints { make in
+            make.right.equalTo(hitBtn.snp.left).offset(-30)
+            make.centerY.equalTo(hitBtn)
+        }
+        
+        view.addSubview(sliderBar)
+        sliderBar.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.width.equalToSuperview().dividedBy(1.1)
+        }
+        
+        view.addSubview(titleLabel)
+        titleLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().offset(100)
+            make.width.equalToSuperview()
+        }
+        
+        view.addSubview(scoreLabel)
+        scoreLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(titleLabel.snp.bottom).offset(50)
+            make.width.lessThanOrEqualToSuperview()
+        }
+        
+        view.addSubview(backgroundImageView)
+        backgroundImageView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.size.equalToSuperview()
+        }
+        
+        view.sendSubviewToBack(backgroundImageView)
     }
     
     func makeHitButton() -> UIButton {
@@ -35,7 +84,7 @@ class ViewController: UIViewController {
         btn.setTitleColor(.systemPink, for: .normal)
         btn.backgroundColor = .systemGray
         btn.layer.cornerRadius = 10
-        btn.setImage(UIImage(systemName: "personalhotspot.circle"), for: .normal)
+        btn.setImage(pointIcon("personalhotspot.circle"), for: .normal)
         btn.tintColor = .systemPink
         
         var config = UIButton.Configuration.plain()
@@ -46,11 +95,82 @@ class ViewController: UIViewController {
         return btn
     }
     
+    func makeSliderBar() -> UISlider {
+        let slider = UISlider()
+        slider.minimumValue = 1
+        slider.maximumValue = 100
+        slider.minimumValueImage = pointIcon("moon.stars")
+        slider.maximumValueImage = pointIcon("moon.stars.fill")
+        slider.isContinuous = false
+        slider.addTarget(self, action: #selector(valueChanged), for: .touchUpInside)
+        return slider
+    }
+    
+    func makeTitleLabel() -> UILabel {
+        var attributes = [NSAttributedString.Key : Any]()
+        attributes[NSAttributedString.Key.font] = UIFont.systemFont(ofSize: 30)
+        let string = NSAttributedString(string: "Put the Bull's Eye as close as you can to:\(randomNumber)", attributes: attributes)
+        let label = UILabel()
+        label.attributedText = string
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }
+    
+    func makeResetButton() -> UIButton {
+        let btn = UIButton()
+        btn.setImage(pointIcon("arrow.counterclockwise", 30), for: .normal)
+        btn.tintColor = .systemPink
+        btn.addTarget(self, action: #selector(resetBtnClicked), for: .touchUpInside)
+        return btn
+    }
+    
+    func makeScoreLabel() -> UILabel {
+        let label = UILabel()
+        label.text = "Current Score: \(totalScore)"
+        label.font = UIFont.systemFont(ofSize: 25)
+        return label
+    }
+    
+    func makeBackgroundImageView() -> UIImageView {
+        let imageview = UIImageView()
+        imageview.image = UIImage(named: "background")
+        return imageview
+    }
+    
     @objc func hitBtnClicked() {
-        let alert = UIAlertController(title: "You almost had it!", message: "Your total score is xxx", preferredStyle: .alert)
-        let action = UIAlertAction(title: "Awesome!", style: .cancel, handler: nil)
+        let num = Int(sliderBar.value)
+        if num != randomNumber {
+            makeAlert("You almost had it! The gap is \(abs(num - randomNumber))!")
+            numberOfAttemps -= 1
+        } else {
+            randomNumber = Int.random(in: 1...100)
+            numberOfAttemps = 10
+            totalScore += max(0, numberOfAttemps)
+            makeAlert("SUCCESSFUL!!!!!!!!!!!!!")
+        }
+    }
+    
+    func makeAlert(_ alertTitle: String, _ actionTitle: String = "Awesome!") {
+        let alert = UIAlertController(title: alertTitle, message: "Your total score is \(totalScore)", preferredStyle: .alert)
+        let action = UIAlertAction(title: actionTitle, style: .cancel, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+    }
+    
+    
+    @objc func valueChanged() {
+        print(Int(sliderBar.value))
+    }
+    
+    @objc func resetBtnClicked() {
+        totalScore = 0
+        randomNumber = Int.random(in: 1...100)
+    }
+    
+    func pointIcon(_ iconName: String, _ pointSize: CGFloat = 22) -> UIImage?{
+        let config = UIImage.SymbolConfiguration(pointSize: pointSize)
+        return UIImage(systemName: iconName, withConfiguration: config)
     }
 }
 
